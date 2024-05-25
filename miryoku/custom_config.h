c65dda0abec4d4558d8d7bb602211be7e1a4b67a
Kteby <sergeyleuhinintegral@gmail.com>
2024-05-25 17:36:01 +0300

    Revert changes

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,20 +1,3 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#include <dt-bindings/zmk/keys.h>
-#include <behaviors.dtsi>
-
-#define AS(keycode) &as LS(keycode) keycode     // Autoshift Macro
-
-/ {
-    behaviors {
-        as: auto_shift {
-            compatible = "zmk,behavior-hold-tap";
-            #binding-cells = <2>;
-            tapping_term_ms = <135>;
-            quick_tap_ms = <0>;
-            flavor = "tap-preferred";
-            bindings = <&kp>, <&kp>;
-        };
-    };
-};
