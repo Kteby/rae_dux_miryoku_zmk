56b0d3da4258a21e67d9365ff41596159dc329e1
Kteby <sergeyleuhinintegral@gmail.com>
2024-05-25 18:14:24 +0300

    Added bunya layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,9 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALTERNATIVES_BASE_BUNYA \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp B,             &kp L,             &kp M,             &kp C,             &kp Z,             &kp J,             &kp F,             &kp O,             &kp U,             &kp COMMA,         \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),    U_MT(LSHFT, D),    &kp P,             &kp Y,             U_MT(LSHFT, H),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I),     \
+U_LT(U_BUTTON, X), U_MT(RALT, Q),     &kp V,             &kp Q,             &kp W,             &kp SEMI,          &kp K,             &kp SQT,           U_MT(RALT, SLASH), U_LT(U_BUTTON, DOT),\
+U_NP,              U_NP,              U_LT(U_MEDIA, DEL),U_LT(U_NAV, S),    U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE),U_LT(U_FUN, BSPC), U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA MIRYOKU_ALTERNATIVES_BASE_BUNYA
