7f57033754f67cab1605194cb8cea4d4f2599476
Kteby <sergeyleuhinintegral@gmail.com>
2024-05-30 22:36:59 +0300

    Added bunya layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,10 +7,4 @@ U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),    U_MT(LSHFT, D),    &kp 
 U_LT(U_BUTTON, X), U_MT(RALT, Q),     &kp V,             &kp G,             &kp W,             &kp SEMI,          &kp K,             &kp SQT,           U_MT(RALT, SLASH), U_LT(U_BUTTON, DOT),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, S),    U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE),U_LT(U_FUN, BSPC), U_NP,              U_NP
 
-#define MIRYOKU_ALTERNATIVES_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
-U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp DEL,           &kp BSPC,           U_NP,              U_NP
-
 #define MIRYOKU_LAYER_EXTRA MIRYOKU_ALTERNATIVES_BASE_BUNYA
